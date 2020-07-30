b1d05d0ab034b02ea82d7a647582679d920762e8
Junior Henrique <juniorhenrique030@outlook.com>
2020-07-30 19:38:09 -0300

    Starting to implement Board Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,15 @@ package application;
 
 import java.util.Scanner;
 
+import boardGame.Board;
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
+		Board board = new Board(8, 8);
 		
 		sc.close();
 	}
---
+++ src/boardGame/Piece.java
@@ -0,0 +1,13 @@
+package boardGame;
+
+public class Piece { // Criando class peça e atribuindo a posição e tabuleiro
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+	}
+	protected Board getBoard() {//uso exclusivo para camada tabuleiro
+		return board;
+	}
+}
