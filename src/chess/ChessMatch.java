6764def88077907813af4ab6e419469f8a12e3ec
Junior Henrique <juniorhenrique030@outlook.com>
2020-08-20 20:09:21 -0300

    Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -10,6 +10,7 @@ import boardGame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 import chess.pieces.king;
 
@@ -195,6 +196,7 @@ public class ChessMatch {
 	}
 
 	public void initialSetup() {
+			placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 			placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 			placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		    placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
@@ -211,6 +213,7 @@ public class ChessMatch {
 	        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+	        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 	        placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 	        placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 	        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
