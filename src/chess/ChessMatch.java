847c9deda282b1d1bb0be176236f5638889450d3
Junior Henrique <juniorhenrique030@outlook.com>
2020-08-20 19:44:34 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardGame.Board;
 import boardGame.Piece;
 import boardGame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
 import chess.pieces.king;
@@ -193,6 +194,8 @@ public class ChessMatch {
 	}
 
 	public void initialSetup() {
+		    placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+		    placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 	        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
 	        placeNewPiece('e', 1, new king(board, Color.WHITE));
 	        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -205,6 +208,8 @@ public class ChessMatch {
 	        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+	        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+		    placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 	        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 	        placeNewPiece('e', 8, new king(board, Color.BLACK));
 	        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
